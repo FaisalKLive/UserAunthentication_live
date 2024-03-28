822b95938871f4d7d7e28946b9622df41d7a2342
FaisalKLive <FaisalKLive@Hotmail.com>
2024-03-28 07:59:43 -0400

    first commit

---
+++ model/userModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import {} from 'dotenv/config'
+
+const uri = process.env.MONGO_URI
+// const uri = "mongodb+srv://FullStack:Conestoga@cluster0.1ua8had.mongodb.net/CostCoUserz?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(uri).then(()=>console.log("***************************Connected to Mongodb ******************************"))
+.catch((err)=>console.log(`##############Not Connected due to error below ##################\n${err}`))
+
+
+const userSchema = mongoose.Schema({
+    name :{type:String,required:true},
+    email :{type:String,required:true},
+    pwd  : {type:String,required:true}
+})
+
+const userModel = mongoose.model('GeneralUser',userSchema)
+
+export default userModel
